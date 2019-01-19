73b068996a8e101c84182dfc022805730d68059d
om <om@bigappcompany.in>
2019-01-19 18:19:42 +0530

    major fix, allow decorators with cra

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './TodoList';
+import store from './TodoStore';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur quidem neque cumque provident quae ullam est facilis officiis maiores. Sint, tenetur obcaecati non natus rerum tempora alias itaque iure reprehenderit.</p>
+        <p>mobx test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+ class TodoList extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <p>this is coming from todolist</p>
+
+      </div>
+    )
+  }
+}
+
+export default TodoList
