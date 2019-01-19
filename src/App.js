8a22a1fec2a1cd8284754a063cbfcb832188c540
om <om@bigappcompany.in>
2019-01-19 18:41:08 +0530

    remove experimental decorator warning

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <TodoList store={store} />
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur quidem neque cumque provident quae ullam est facilis officiis maiores. Sint, tenetur obcaecati non natus rerum tempora alias itaque iure reprehenderit.</p>
         <p>mobx test</p>
       </div>
